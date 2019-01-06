8b630f12d521e5399670418d47b156b40d640ba8
mglavall <mgomezlavall@gmail.com>
2019-01-06 15:22:30 +0100

    Add author name when hovering the image

--- src/components/Post/Post.js
+++ src/components/Post/Post.js
@@ -1,19 +1,43 @@
-import React from "react";
+import React, { Component } from "react";
 import "./Post.scss";
 import userPhoto from "../../assets/user1.jpg";
 
-const post = props => (
-  <div className="d-flex flex-wrap mb-2">
-    <div className="col-1">
-      <img className="author-img mb-1" alt="Author" src={userPhoto} />
-    </div>
-    <article className="post col-11 p-2">
-      <div className="text-size-2 mb-2">{props.title}</div>
-      <div>
-        <div>{props.body}</div>
-      </div>
-    </article>
-  </div>
-);
+class Post extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      hoveredImg: false
+    };
+  }
+  onImgMouseEnterHandler() {
+    this.setState({ hoveredImg: true });
+  }
 
-export default post;
+  onImgMouseLeaveHandler() {
+    this.setState({ hoveredImg: false });
+  }
+  render() {
+    return (
+      <div className="d-flex flex-wrap mb-2">
+        <div className="col-1">
+          <img
+            className="author-img mb-1"
+            alt="Author"
+            src={userPhoto}
+            onMouseEnter={this.onImgMouseEnterHandler.bind(this)}
+            onMouseLeave={this.onImgMouseLeaveHandler.bind(this)}
+          />
+          <span className={"author-name" + (this.state.hoveredImg ? " hovered" : "")}>Alicia</span>
+        </div>
+        <article className="post col-11 p-2">
+          <div className="text-size-2 mb-2">{this.props.title}</div>
+          <div>
+            <div>{this.props.body}</div>
+          </div>
+        </article>
+      </div>
+    );
+  }
+}
+
+export default Post;
