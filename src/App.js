de57c7249b2032d7d8452d75954e71ba300a44dd
mglavall <mgomezlavall@gmail.com>
2019-01-06 14:08:51 +0100

    Add responsiveness to the post
    The image moves to the top of the post when the window is small

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1 className="align-center title"> the post list </h1>
+        <h1 className="align-center title"> THE POST LIST </h1>
         <PostList />
       </div>
     );
--- src/components/Post/Post.js
+++ src/components/Post/Post.js
@@ -3,9 +3,9 @@ import "./Post.scss";
 import userPhoto from "../../assets/user1.jpg";
 
 const post = props => (
-  <div className="d-flex mb-2">
+  <div className="d-flex flex-wrap mb-2">
     <div className="col-1">
-      <img className="author-img" alt="Author" src={userPhoto} />
+      <img className="author-img mb-1" alt="Author" src={userPhoto} />
     </div>
     <article className="post col-11 p-2">
       <div className="text-size-2 mb-2">{props.title}</div>
